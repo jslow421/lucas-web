cb3406c3fce8a3218569372ac70234e4629a31b7
John Slowik <john@jslowik.com>
2025-05-20 12:04:30 -0500

    Update case study card desktop view

--- website/components/case-studies/buckbuckley.tsx
+++ website/components/case-studies/buckbuckley.tsx
@@ -5,25 +5,32 @@ import Card from "../card";
 export default function BuckBuckleyCaseStudy() {
   return (
     <Card>
-      <div className="p-4">Video Work With Buck Buckley&apos;s</div>
-      <div className="p-4">
-        A series of engaging videos created for Klaus Roofing Systems by Buck
-        Buckley&apos;s combined customer testimonials and educational content to
-        highlight their innovative roofing solutions. These videos garnered
-        thousands of views, drove a 3% increase in sales, and enhanced customer
-        trust, showcasing the power of storytelling in building brand awareness
-        and driving growth.
-      </div>
-      <LinkButton href="#" text="Case Study" />
-      <div className="p-4">
-        <Image
-          src="/buck-buckley-case-study.jpeg"
-          alt="MRS Logo"
-          width={128}
-          height={128}
-          className="object-cover mb-4"
-          priority
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="flex flex-col justify-center">
+          <div className="p-4">Video Work With Buck Buckley&apos;s</div>
+          <div className="p-4">
+            A series of engaging videos created for Klaus Roofing Systems by
+            Buck Buckley&apos;s combined customer testimonials and educational
+            content to highlight their innovative roofing solutions. These
+            videos garnered thousands of views, drove a 3% increase in sales,
+            and enhanced customer trust, showcasing the power of storytelling in
+            building brand awareness and driving growth.
+          </div>
+          <div className="p-4">
+            <LinkButton href="#" text="Case Study" />
+          </div>
+        </div>
+        <div className="flex items-center justify-center p-4">
+          <div className="relative w-full max-w-[256px] aspect-square">
+            <Image
+              src="/buck-buckley-case-study.jpeg"
+              alt="MRS Logo"
+              fill
+              className="object-cover mb-4"
+              priority
+            />
+          </div>
+        </div>
       </div>
     </Card>
   );
--- website/components/case-studies/mrs.tsx
+++ website/components/case-studies/mrs.tsx
@@ -5,25 +5,34 @@ import Card from "../card";
 export default function MrsCaseStudy() {
   return (
     <Card>
-      <div className="p-4">Midwest Refrigerated Services Brand Guidelines</div>
-      <div className="p-4">
-        Branding guidelines for MRS ensure consistency across all marketing and
-        communication efforts, reflecting the company&apos;s values of Modesty,
-        Integrity, Dependability, Work Ethic, Efficiency, Safety, and Trust.
-        These guidelines are vital for building a cohesive and recognizable
-        brand identity that fosters trust and loyalty among customers and
-        employees.
-      </div>
-      <LinkButton href="#" text="Case Study" />
-      <div className="p-4">
-        <Image
-          src="/mrs-case-study.jpeg"
-          alt="MRS Logo"
-          width={128}
-          height={128}
-          className="object-cover mb-4"
-          priority
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="flex flex-col justify-center">
+          <div className="p-4">
+            Midwest Refrigerated Services Brand Guidelines
+          </div>
+          <div className="p-4">
+            Branding guidelines for MRS ensure consistency across all marketing
+            and communication efforts, reflecting the company&apos;s values of
+            Modesty, Integrity, Dependability, Work Ethic, Efficiency, Safety,
+            and Trust. These guidelines are vital for building a cohesive and
+            recognizable brand identity that fosters trust and loyalty among
+            customers and employees.
+          </div>
+          <div className="p-4">
+            <LinkButton href="#" text="Case Study" />
+          </div>
+        </div>
+        <div className="flex items-center justify-center p-4">
+          <div className="relative w-full max-w-[256px] aspect-square">
+            <Image
+              src="/mrs-case-study.jpeg"
+              alt="MRS Logo"
+              fill
+              className="object-cover mb-4"
+              priority
+            />
+          </div>
+        </div>
       </div>
     </Card>
   );
